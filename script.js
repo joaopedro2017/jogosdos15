ec2a560633432cbee02396555bcd9bda0d019af4
João Pedro <jguedespresto@gmail.com>
2017-03-05 10:07:07 -0300

    Adiciona função Render

--- script.js
+++ script.js
@@ -7,4 +7,24 @@
 	}
 	casas.push(null);
 	render();
+
+	function render(){
+		for(var i in casas){
+			var casa = casas[i];
+			if(casa){
+				casa.style.left = (i%4) * 50 + 5 + "px";
+				if(i<4){
+					casa.style.top = "5px";
+				}else{
+					if(i<8){
+						casa.style.top = "55px";
+					}else if(i<12){
+						casa.style.top = "155px";
+					}else{
+						casa.style.top = "205px"
+					}
+				}
+			}
+		}
+	}
 })
