0a4fe9085d9e4d7af1676d780f80f793fe7670ef
João Pedro <jguedespresto@gmail.com>
2017-03-05 10:26:10 -0300

    Não deu certo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 	var casas = [];
 	for(var i = 1; i < 16; i++){
 		var casa = document.querySelector("#n"+i);
-		casa.addEventListener("click", moveTile,false);
+		casa.addEventListener("click", moveCasa,false);
 		casas.push(casa);
 	}
 	casas.push(null);
@@ -27,4 +27,38 @@
 			}
 		}
 	}
-})
+
+	function moveCasa(){
+		var index = casas.indexOf(this);
+		//move esquerda
+		if(index % 4 !== 0){
+			if(!casas[index-1]){
+				casas[index-1] = this;
+				casas[index] = null;
+			}
+		}
+		//move direita
+		if(index % 4 !== 2){
+			if(!casas[index+1]){
+				casas[index+1] = this;
+				casas[index] = null;
+			}
+		}
+		//move cima
+		if(index > 3){
+			if(!casas[index-3]){
+				casas[index-3] = this;
+				casas[index] = null;
+			}
+		}
+		//move baixo
+		if(index < 12){
+			if(!casas[index+3]){
+				casas[index+3] = this;
+				casas[index] = null;
+			}
+		}
+		render();
+	}
+
+}());
